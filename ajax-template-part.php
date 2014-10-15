54dec5799b533e645775f52068cdb877b08358b3
Giuseppe Mazzapica <giuseppe.mazzapica@gmail.com>
2014-10-15 09:35:56 +0200

    Added surname in license info

--- ajax-template-part.php
+++ ajax-template-part.php
@@ -9,7 +9,7 @@
  */
 
 /*
-  Copyright (C) 2014 Giuseppe
+  Copyright (C) 2014 Giuseppe Mazzapica
 
   This program is free software; you can redistribute it and/or
   modify it under the terms of the GNU General Public License
